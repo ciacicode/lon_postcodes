3a4ed81be85f86ef14c7923fe8905018ca588da2
Maria Cerase <maria.cerase@gmail.com>
2015-07-26 19:37:32 +0100

    applied pep8

--- postcode_parser.py
+++ postcode_parser.py
@@ -9,19 +9,21 @@ WESTMINSTER_CODE_REGEX = re.compile('^(?P<code>W1)[^[0-9]]')
 # The postcode_parser contains a set of functions built with the goal of creating a csv containing
 # geometry data for all London postcodes
 
+
 def select_london_postcodes():
     """
     Uses the ukpostcodes.csv courtesy of https://github.com/Gibbs/UK-Postcodes/blob/master/postcodes.csv
     and outputs a list of postcodes only for the greater london area
     """
-    with open ('ukpostcodes.csv') as csv_file:
+    with open('ukpostcodes.csv') as csv_file:
         reader = csv.DictReader(csv_file)
         output_list = []
-        london_areas = ['Greater London', 'Westminster','City of London']
+        london_areas = ['Greater London', 'Westminster', 'City of London']
         for row in reader:
             if row['region'] in london_areas:
                 output_list.append(row['postcode'])
     return output_list
+
 
 def select_london_postcodes_geometry():
     """
@@ -38,16 +40,15 @@ def select_london_postcodes_geometry():
     uk_geometry = json_data['features']
     london_postcodes = select_london_postcodes()
 
-
     # for each feature in the features list access the name property
     # and compare it with the london_postcodes
-    for item in range(0,len(uk_geometry)):
+    for item in range(0, len(uk_geometry)):
         # feature is a postcode dict containing data and name
         feature = uk_geometry[item]
         postcode = feature['properties'].values()
+        pdb.set_trace()
         postcode = str(postcode[0])
-
-        #normalise the postcode so to get only the area
+        # normalise the postcode so to get only the area
         complete_postcode_area = postcode.split()[0]
         postcode_area = complete_postcode_area
 
@@ -56,7 +57,6 @@ def select_london_postcodes_geometry():
                 '\g<code>', postcode_area)
 
         if postcode_area in london_postcodes:
-            print complete_postcode_area
             row_dict['parent'] = complete_postcode_area
             row_dict['name'] = postcode
             geometry = feature['geometry']
@@ -72,24 +72,19 @@ def select_london_postcodes_geometry():
             row_dict = {}
     return final_list
 
+
 def create_lon_postcodes_geometry(geometry):
     """
     Receives a list of geometry data containing dicts of latitude and longitude
     Creates a csv for easy reference and mapping
     """
     with open('london_postcodes.csv', 'w') as csvfile:
-        fieldnames = ['parent','name', 'lons', 'lats']
+        fieldnames = ['parent', 'name', 'lons', 'lats']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for postcode in range(0,len(geometry)):
+        for postcode in range(0, len(geometry)):
             writer.writerow(geometry[postcode])
 
 london_geometry_list = select_london_postcodes_geometry()
 
 create_lon_postcodes_geometry(london_geometry_list)
-
-
-
-
-
-
